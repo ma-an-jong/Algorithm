e543745cccce6e80fe1101c4cb53c283572ecc15
gimminjong <alswhd1113@gmail.com>
2022-12-26 20:43:29 +0900

    feat(괄호) : 길이가 L인 짝이 맞는 괄호의 수를 세는 문제
    description : 시작은 항상 여는 괄호, 짝이 맞는 닫는 괄호의 인덱스를 i라 생각하고
    i-2 * L-i 의 모든 합을 계산

--- Success/괄호.java
+++ Success/괄호.java
@@ -1,74 +1,53 @@
-package Success;
+package Algorithm.Trying;
+
+import Algorithm.Stub;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Stack;
 
+public class ê´„í˜¸ {
 
-public class °ýÈ£ {
-	public static void main(String[] args) throws Exception {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int N = Integer.parseInt(br.readLine());
-		String str;
-		
-		for(int i = 0 ; i < N ; i++)
-		{
-			str = br.readLine();
-			boolean flag = true;
-			
-			Stack<Character> stack = new Stack<Character>();
-			
-			for(int j = 0; j < str.length() ; j++)
-			{
-				
-				char element = str.charAt(j);
-				
-				try
-				{
-					char top = stack.peek();
-					
-					if(top != element)
-					{
-						stack.pop();
-					}
-					else
-					{
-						stack.push(element);
-					}
-				}
-				catch (Exception e) 
-				{
-					if(element != '(')
-					{
-						flag = false;
-						break;
-					}
-					else
-					{
-						stack.push(element);
-					}
-				}
-				
-			}
-			
-			if(!flag || stack.size() != 0) 
-			{
-				bw.write("NO\n");
-			}
-			else if(stack.size() == 0)
-			{
-				bw.write("YES\n");
-			}
-		
-		}
-		
-		bw.flush();
-		
-	}
+    static long foo(int l){
 
+        if(l <= 0) return 1;
+        if(l % 2 == 1) return 0;
+
+        if(dp[l] != 0){
+            return dp[l];
+        }
+
+        for(int i = 2; i <= l;i++)
+        {
+            dp[l] += (foo(i-2)*foo(l-i))%DIVISION;
+            dp[l] %= DIVISION;
+        }
+
+        return dp[l];
+    }
+
+    static final int DIVISION = 1_000_000_007;
+    static long[] dp = new long[5001];
+    public static void main(String[] args) throws Exception {
+        Stub.stub();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        dp[2] = 1;
+        for(int t = 0 ; t < T ; t++){
+            int l = Integer.parseInt(br.readLine());
+
+            if(l % 2 == 1) {
+                bw.write("0\n");
+                continue;
+            }
+
+            bw.write(foo(l)+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
 }
